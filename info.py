7ed26b6fa20234bc6a6156744aa016ba8ab0f172
Nsmovies <140789218+Nsmovies@users.noreply.github.com>
2025-07-25 17:03:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,11 +28,11 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://envs.sh/gle.jpg')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://i.ibb.co/RGfRT2jk/photo-2025-01-23-05-42-03-7530477859291267076.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/60e8a622b14796e4448ce.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/gYG.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://i.ibb.co/HpNcwvzR/photo-2025-05-06-15-41-18-7530474547871481880.jpg'))
 FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
 
 # ============================
