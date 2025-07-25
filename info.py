ce1433a84699aed544508db19aa4941d568fe8ae
Nsmovies <140789218+Nsmovies@users.noreply.github.com>
2025-07-25 20:27:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -176,9 +176,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://aggressive-kristal-chayadevi-2be51b42.koyeb.app/".format(FQDN)
+    URL = "https://youngest-sharline-chayadevi-f930aeff.koyeb.app/".format(FQDN)
 else:
-    URL = "https://aggressive-kristal-chayadevi-2be51b42.koyeb.app/".format(FQDN)
+    URL = "https://youngest-sharline-chayadevi-f930aeff.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
